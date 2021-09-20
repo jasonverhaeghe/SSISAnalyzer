42ddf0152eebcfc7eb9efcb23fb0cd05b33d4514
Jason Verhaeghe <jason.verhaeghe@picanol.be>
2021-09-20 15:04:02 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtTables = new System.Windows.Forms.TextBox();
+            this.btnCheckTables = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDirectoryToSave = new System.Windows.Forms.TextBox();
+            this.btnBrowseLocationToSaveFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(40, 44);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(207, 13);
+            this.lblDirectory.TabIndex = 3;
+            this.lblDirectory.Text = "Directory where the SSIS packages reside";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(645, 38);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(32, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(253, 41);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(386, 20);
+            this.txtDirectory.TabIndex = 5;
+            this.txtDirectory.Text = "C:\\TEMP\\BI ETL.SSIS.DWH_Facts";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(683, 38);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(150, 23);
+            this.btnProcess.TabIndex = 4;
+            this.btnProcess.Text = "Process SSIS packages";
+            this.toolTip1.SetToolTip(this.btnProcess, "The Data Flow Task, Execute SQL tasks will be analysed on embedded SQL and the SQ" +
+        "L code will be put into text files...");
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtTables
+            // 
+            this.txtTables.Location = new System.Drawing.Point(43, 96);
+            this.txtTables.Multiline = true;
+            this.txtTables.Name = "txtTables";
+            this.txtTables.Size = new System.Drawing.Size(634, 228);
+            this.txtTables.TabIndex = 7;
+            // 
+            // btnCheckTables
+            // 
+            this.btnCheckTables.Location = new System.Drawing.Point(683, 301);
+            this.btnCheckTables.Name = "btnCheckTables";
+            this.btnCheckTables.Size = new System.Drawing.Size(150, 23);
+            this.btnCheckTables.TabIndex = 8;
+            this.btnCheckTables.Text = "Check tables";
+            this.toolTip1.SetToolTip(this.btnCheckTables, "Checks if the table exists inside the SSIS package (either in embedded SQL or ins" +
+        "ide the controls)");
+            this.btnCheckTables.UseVisualStyleBackColor = true;
+            this.btnCheckTables.Click += new System.EventHandler(this.btnCheckTables_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Paste a list of tables you want to check versus your SSIS packages";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 1000;
+            this.toolTip1.ReshowDelay = 500;
+            this.toolTip1.ShowAlways = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Directory to save the text files";
+            // 
+            // txtDirectoryToSave
+            // 
+            this.txtDirectoryToSave.Location = new System.Drawing.Point(253, 11);
+            this.txtDirectoryToSave.Name = "txtDirectoryToSave";
+            this.txtDirectoryToSave.Size = new System.Drawing.Size(386, 20);
+            this.txtDirectoryToSave.TabIndex = 11;
+            this.txtDirectoryToSave.Text = "C:\\TEMP\\";
+            // 
+            // btnBrowseLocationToSaveFiles
+            // 
+            this.btnBrowseLocationToSaveFiles.Location = new System.Drawing.Point(645, 8);
+            this.btnBrowseLocationToSaveFiles.Name = "btnBrowseLocationToSaveFiles";
+            this.btnBrowseLocationToSaveFiles.Size = new System.Drawing.Size(32, 23);
+            this.btnBrowseLocationToSaveFiles.TabIndex = 12;
+            this.btnBrowseLocationToSaveFiles.Text = "...";
+            this.btnBrowseLocationToSaveFiles.UseVisualStyleBackColor = true;
+            this.btnBrowseLocationToSaveFiles.Click += new System.EventHandler(this.btnBrowseLocationToSaveFiles_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(897, 360);
+            this.Controls.Add(this.btnBrowseLocationToSaveFiles);
+            this.Controls.Add(this.txtDirectoryToSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheckTables);
+            this.Controls.Add(this.txtTables);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lblDirectory);
+            this.Name = "Form1";
+            this.Text = "SSIS Package analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+   
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtTables;
+        private System.Windows.Forms.Button btnCheckTables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDirectoryToSave;
+        private System.Windows.Forms.Button btnBrowseLocationToSaveFiles;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.SqlServer;
+using System.Xml;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static string SSISPackage = "";
+        public static string HashString = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtDirectory.Text = diag.SelectedPath;  //selected folder path
+
+            }
+
+        }
+        public void ProcessNodes(XmlNode node, XmlNamespaceManager nsmgr, TextWriter txt) //recursive in case of sequence containers
+        {
+            foreach (XmlNode executable in node)
+            {
+                //XmlNodeList objectdata;
+
+                string exec = executable.Attributes.GetNamedItem("DTS:CreationName").Value.ToString();
+                switch (exec)
+                {
+                    case var _ when exec.Contains("ExecuteSQLTask")://Execute SQL Task
+                        txt.WriteLine("--" + executable.Attributes.GetNamedItem("DTS:ObjectName").Value.ToString());
+                        ProcessExecuteSQLTask(executable, nsmgr, txt);
+                        break;
+                    case var _ when exec.Contains("STOCK:SEQUENCE"):// "Sequence Container"
+                        txt.WriteLine("--" + executable.Attributes.GetNamedItem("DTS:ObjectName").Value.ToString());
+                        ProcessNodes(executable.SelectSingleNode("DTS:Executables", nsmgr), nsmgr, txt);
+                        break;
+                    case var _ when exec.Contains("SSIS.Pipeline.3"):// "Data Flow Task"
+                        HashString = "";
+                        txt.WriteLine("--" + executable.Attributes.GetNamedItem("DTS:ObjectName").Value.ToString());
+                        ProcessDataFlowTask(executable, nsmgr, txt);
+                        break;
+
+                }
+
+            }
+
+
+        }
+
+        static void ProcessExecuteSQLTask(XmlNode executable, XmlNamespaceManager nsmgr, TextWriter txt)
+        {
+            string SqlStatementSource = executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("SQLTask:SqlTaskData", nsmgr).Attributes.GetNamedItem("SQLTask:SqlStatementSource").Value.ToString();
+            //if the SQL statement does not contain an EXEC statement then it is a regular SQL expression and it should be converted to a stored proc
+            if (SqlStatementSource.Contains("exec") != true)
+            {
+
+                txt.WriteLine("SET ANSI_NULLS ON");
+                txt.WriteLine("--USE " + executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("SQLTask:SqlTaskData", nsmgr).Attributes.GetNamedItem("SQLTask:Connection").Value.ToString());
+                txt.WriteLine("GO");
+                txt.WriteLine("SET QUOTED_IDENTIFIER ON");
+                txt.WriteLine("GO");
+                txt.WriteLine("/************************************************************************/");
+                txt.WriteLine("/*	Created by:		Jason Verhaeghe										*/");
+                txt.WriteLine("/*   Creation date:	" + DateTime.Now.ToString("yyyy-MM-dd") + "	    	*/");
+                txt.WriteLine("/*	Updated by:												            */");
+                txt.WriteLine("/*	Update date													        */");
+                txt.WriteLine("/*	SSIS PAckage:   " + SSISPackage.ToString() + "                      */");
+                txt.WriteLine("/*	Description:    " + executable.Attributes.GetNamedItem("DTS:ObjectName").Value.ToString() + "  */");
+                txt.WriteLine("/*	Version:															*/");
+                txt.WriteLine("/*	v0                                              					*/");
+                txt.WriteLine("/*	v1                                              					*/");
+                txt.WriteLine("/*	add v1,v2 with change info											*/");
+                txt.WriteLine("/************************************************************************/");
+                txt.WriteLine("--" + SqlStatementSource.Replace("\r\n", " ").Replace("\n", " ").Replace("\r", " "));
+                txt.WriteLine("CREATE PROC ssis.usp_" + SSISPackage.ToString() + "_" + executable.Attributes.GetNamedItem("DTS:ObjectName").Value.ToString().Replace(" ", "_"));
+                txt.WriteLine("AS");
+                txt.WriteLine("BEGIN");
+                txt.WriteLine(SqlStatementSource);
+                txt.WriteLine("END");
+
+            }
+            //else
+            //{
+            //    txt.WriteLine("--"+ SqlStatementSource.Replace("\r\n", " ").Replace("\n", " ").Replace("\r", " "));
+            //}
+            //XmlNodeList ParameterBindingsList = executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("SQLTask:SqlTaskData", nsmgr).SelectNodes("SQLTask:ParameterBinding", nsmgr);
+            //if (ParameterBindingsList != null)
+            //{
+            //    foreach (XmlNode pb in ParameterBindingsList)
+            //    {
+            //        txt.WriteLine("--Parameter " +
+            //            pb.Attributes.GetNamedItem("SQLTask:ParameterName").Value.ToString() + " " +
+            //            pb.Attributes.GetNamedItem("SQLTask:DtsVariableName").Value.ToString() + " " +
+            //            pb.Attributes.GetNamedItem("SQLTask:ParameterDirection").Value.ToString()
+            //         );
+
+            //    }
+            //}
+            //XmlNodeList ResultBindingsList = executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("SQLTask:SqlTaskData", nsmgr).SelectNodes("SQLTask:ResultBinding", nsmgr);
+            //if (ResultBindingsList != null)
+            //{
+            //    foreach (XmlNode pb in ResultBindingsList)
+            //    {
+            //        txt.WriteLine("--Result " +
+            //            pb.Attributes.GetNamedItem("SQLTask:ResultName").Value.ToString() + " " +
+            //            pb.Attributes.GetNamedItem("SQLTask:DtsVariableName").Value.ToString()
+            //         );
+
+            //    }
+            //}
+        }
+        static void ProcessDataFlowTask(XmlNode executable, XmlNamespaceManager nsmgr, TextWriter txt)
+        {
+            if (executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("pipeline", nsmgr).HasChildNodes)
+            {
+                XmlNodeList ComponentsList = executable.SelectSingleNode("DTS:ObjectData", nsmgr).SelectSingleNode("pipeline", nsmgr).SelectSingleNode("components", nsmgr).SelectNodes("component");
+                if (ComponentsList != null)
+                {
+                    foreach (XmlNode comp in ComponentsList)
+                    {
+                        switch (comp.Attributes.GetNamedItem("componentClassID").Value.ToString())
+                        {
+                            case "DTS.ManagedComponentWrapper.3":
+                                //we have a multi hash component
+                                if (comp.Attributes.GetNamedItem("contactInfo").Value.ToString() == "http://ssismhash.codeplex.com/")
+                                {
+
+                                    XmlNodeList HashInputList = comp.SelectSingleNode("inputs").SelectSingleNode("input").SelectSingleNode("inputColumns").SelectNodes("inputColumn");
+                                    foreach (XmlNode prop in HashInputList)
+                                    {
+                                        HashString += "--,ISNULL([" + prop.Attributes.GetNamedItem("cachedName").Value.ToString() + "],N'')" + "\r\n";
+                                    }
+                                    HashString = "--,T_NewHashValue = UPPER(CONVERT(NVARCHAR(32),HashBytes('MD5', LTRIM(RTRIM(CONCAT(" + HashString.Substring(3);
+                                    HashString += "--)))),2))";
+                                    txt.WriteLine(HashString);
+                                    //XmlNodeList HashOutputList = comp.SelectSingleNode("outputs").SelectSingleNode("output").SelectSingleNode("outputColumns").SelectSingleNode("outputColumn").SelectSingleNode("properties").SelectNodes("property");
+                                    //foreach (XmlNode prop in HashOutputList)
+                                    //{
+                                    //    switch (prop.Attributes.GetNamedItem("name").Value.ToString())
+                                    //    {
+                                    //        case "InputColumnLineageIDs":
+                                    //            break;
+
+                                    //    }
+                                    //}
+                                }
+                                break;
+                            case "DTSAdapter.OLEDBSource.3":
+                                txt.WriteLine("--Source component name " + comp.Attributes.GetNamedItem("refId").Value.ToString());
+                                XmlNodeList ConnectionsList = comp.SelectSingleNode("connections").SelectNodes("connection");
+                                foreach (XmlNode cn in ConnectionsList)
+                                {
+                                    txt.WriteLine("--Source component Database Connection " + cn.Attributes.GetNamedItem("connectionManagerRefId").Value.ToString());
+                                    txt.WriteLine("USE " + cn.Attributes.GetNamedItem("connectionManagerRefId").Value.ToString().Replace("]", "").Replace("Project.ConnectionManagers[OLEDB_", "").Replace("Project.ConnectionManagers[OLEDB ", ""));
+
+
+
+                                }
+
+                                XmlNodeList CompPropList = comp.SelectSingleNode("properties").SelectNodes("property");
+                                foreach (XmlNode prop in CompPropList)
+                                {
+                                    switch (prop.Attributes.GetNamedItem("name").Value.ToString())
+                                    {
+                                        case "SqlCommand":
+                                            if (prop.HasChildNodes)
+                                            {
+                                                txt.WriteLine("SET ANSI_NULLS ON");
+                                                txt.WriteLine("GO");
+                                                txt.WriteLine("SET QUOTED_IDENTIFIER ON");
+                                                txt.WriteLine("GO");
+                                                txt.WriteLine("/************************************************************************/");
+                                                txt.WriteLine("/*	Created by:		Jason Verhaeghe										*/");
+                                                txt.WriteLine("/*   Creation date:	" + DateTime.Now.ToString("yyyy-MM-dd") + "	    	*/");
+                                                txt.WriteLine("/*	Updated by:												            */");
+                                                txt.WriteLine("/*	Update date													        */");
+                                                txt.WriteLine("/*	SSIS PAckage:   " + SSISPackage.ToString() + "                      */");
+                                                txt.WriteLine("/*	Description:    " + comp.Attributes.GetNamedItem("refId").Value.ToString() + "  */");
+                                                txt.WriteLine("/*	Version:															*/");
+                                                txt.WriteLine("/*	v0                                              					*/");
+                                                txt.WriteLine("/*	v1                                              					*/");
+                                                txt.WriteLine("/*	add v1,v2 with change info											*/");
+                                                txt.WriteLine("/************************************************************************/");
+                                                txt.WriteLine("CREATE PROC ssis.usp_" + SSISPackage.ToString() + "_" + comp.Attributes.GetNamedItem("name").Value.ToString().Replace(" ", "_"));
+                                                if (prop.ChildNodes[0].Value.ToString().Contains("declare @MasterPackageId int = ?"))
+                                                {
+                                                    txt.WriteLine("@MasterPackageId int");
+                                                }
+                                                txt.WriteLine("AS");
+                                                txt.WriteLine("BEGIN");
+                                                txt.WriteLine(prop.ChildNodes[0].Value.ToString().Replace("declare @MasterPackageId int = ?", "--declare @MasterPackageId int = ?"));
+                                                txt.WriteLine("END");
+
+                                            }
+                                            break;
+                                        case "SqlCommandVariable":
+                                            if (prop.HasChildNodes)
+                                            {
+                                                txt.WriteLine("--Source component SQL Command Variable " + prop.ChildNodes[0].Value.ToString());
+                                            }
+                                            break;
+                                    }
+                                }
+                                break;
+
+                        }
+
+
+
+                    }
+                }
+            }
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (txtDirectory.Text != "" && txtTables.Text != "")
+            {
+                try
+                {
+                    string[] dirs = Directory.GetFiles(txtDirectory.Text, "*.dtsx", SearchOption.TopDirectoryOnly);
+                    Console.WriteLine("The number of files starting with c is {0}.", dirs.Length);
+                    foreach (string dir in dirs)
+                    {
+                        SSISPackage = Path.GetFileName(dir.ToString()).ToString().Replace(".dtsx", "");
+                        System.IO.TextWriter writeFile = new StreamWriter("c:\\Temp\\Stored procs " + SSISPackage + ".txt", false);
+                        writeFile.WriteLine("//////Start analysis of SSIS package " + dir.ToString() + "//////");
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(dir);
+                        // Create an XmlNamespaceManager to resolve the default namespace.
+                        XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                        nsmgr.AddNamespace("DTS", "www.microsoft.com/SqlServer/Dts");
+                        nsmgr.AddNamespace("SQLTask", "www.microsoft.com/sqlserver/dts/tasks/sqltask");
+
+                        // Get DTSPackages to add a package
+                        XmlNode DTSExecutables = doc.SelectSingleNode("//DTS:Executables", nsmgr);
+                        ProcessNodes(DTSExecutables, nsmgr, writeFile);
+
+                        writeFile.WriteLine("//////End analysis of SSIS package " + dir.ToString() + "//////");
+                        writeFile.Flush();
+                        writeFile.Close();
+                        writeFile = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The process failed: {0}", ex.ToString());
+                }
+            }
+
+        }
+
+        private void btnCheckTables_Click(object sender, EventArgs e)
+        {
+            string[] stringSeparators = new string[] { "\r\n" };
+
+            string[] allLines = txtTables.Text.Split(stringSeparators, StringSplitOptions.None);
+            int i = 0;
+            string TablesInPackage = "";
+            if (txtDirectory.Text != "" && txtTables.Text != "")
+            {
+                try
+                {
+                    string[] dirs = Directory.GetFiles(txtDirectory.Text, "*.dtsx", SearchOption.TopDirectoryOnly);
+                    System.IO.TextWriter writeFile = new StreamWriter("c:\\Temp\\Packages DPO.txt", false);
+                    foreach (string dir in dirs)
+                    {
+                        SSISPackage = Path.GetFileName(dir.ToString()).ToString().Replace(".dtsx", "");
+                        Console.WriteLine("Processing " + SSISPackage.ToString());
+                        foreach (string line in System.IO.File.ReadAllLines(dir))
+                        {
+                            foreach (string text in allLines)
+                            {
+                                if (text != "")
+                                {
+                                    Console.WriteLine("Searching for " + text.ToString());
+                                    if (line.Contains(text) && text != "")
+                                    {
+                                        i = 1;
+                                        writeFile.WriteLine(SSISPackage + ";" + text + "\r\n");
+
+                                        TablesInPackage += text + "; ";
+                                    }
+                                }
+                            }
+
+
+                        }
+                        if (i == 0)
+                        {
+                            writeFile.WriteLine(SSISPackage);
+
+
+                        }
+                        else
+                        {
+                            writeFile.WriteLine(SSISPackage + ";" + TablesInPackage);
+                            //Console.WriteLine("Processing " + SSISPackage.ToString());
+                        }
+                        TablesInPackage = "";
+                        i = 0;
+                    }
+                    writeFile.Flush();
+                    writeFile.Close();
+                    writeFile = null;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The process failed: {0}", ex.ToString());
+                }
+            }
+        }
+
+        private void btnBrowseLocationToSaveFiles_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtDirectoryToSave.Text = diag.SelectedPath;  //selected folder path
+
+            }
+        }
+    }
+
+}
